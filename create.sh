f69b4353362e6185addc0a5a3ffd2369f30f35a8
Paul Hirsch <paul.hirsch@gsa.gov>
2021-04-06 17:41:42 -0500

    Day 1 - EKS with GitLab over port forwarding

--- create.sh
+++ create.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 eksctl create cluster -f gitlabcluster.yml
+
+helm repo add gitlab https://charts.gitlab.io/
+helm install gitlab gitlab/gitlab -f gitlab_values.yml
+
+echo "The root password is...."
+kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+
+echo "Port forward like so..."
+echo "kubectl port-forward service/gitlab-webservice-default 8181:8181"
+
