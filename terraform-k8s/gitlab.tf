30cd36e67a439238bcce403d6d55b090c0a6306b
timothy-spencer <timothy.spencer@gsa.gov>
2021-04-13 15:39:52 -0700

    move gitlab over too

--- terraform-k8s/gitlab.tf
+++ terraform-k8s/gitlab.tf
@@ -19,34 +19,3 @@ resource "kubernetes_config_map" "gitlab-terraform-info" {
     "certmanager-issuer-email" = var.certmanager-issuer
   }
 }
-
-resource "helm_release" "gitlab" {
-  name       = "gitlab"
-  repository = "https://charts.gitlab.io/"
-  chart      = "gitlab"
-  version    = "4.10.2"
-  namespace  = "gitlab"
-  depends_on = [kubernetes_namespace.gitlab]
-
-  set {
-    name  = "global.hosts.hostSuffix"
-    value = var.cluster_name
-  }
-
-  set {
-    name  = "global.hosts.domain"
-    value = var.domain
-  }
-
-  # we are using teleport to get into the GUI, so don't expose it.
-  # XXX probably will need to turn this off for git ssh
-  set {
-    name  = "global.ingress.enabled"
-    value = false
-  }
-
-  set {
-    name  = "certmanager-issuer.email"
-    value = var.certmanager-issuer
-  }
-}
