881717eaf79db59687123416c7246d1ae87d3425
timothy-spencer <timothy.spencer@gsa.gov>
2021-04-13 17:02:29 -0700

    move autoscaler over to flux

--- terraform-k8s/albingress.tf
+++ terraform-k8s/albingress.tf
@@ -238,13 +238,14 @@ resource "kubernetes_service_account" "aws-load-balancer-controller" {
 }
 
 # This configmap is where we can pass stuff into flux/helm from terraform
-resource "kubernetes_config_map" "terraform-alb-info" {
+resource "kubernetes_config_map" "terraform-info" {
   metadata {
-    name      = "terraform-alb-info"
+    name      = "terraform-info"
     namespace = "kube-system"
   }
 
   data = {
-    "cluster_name" = var.cluster_name
+    "cluster_name" = var.cluster_name,
+    "region" = var.region
   }
 }
