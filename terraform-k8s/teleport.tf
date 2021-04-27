1f3c28c6af41a756cd3a1de6da3959f313afc8de
Tim Spencer <timothy.spencer@gsa.gov>
2021-04-27 14:15:09 -0700

    Get teleport kubernetes working (#13)
    * move to use this branch
    
    * try restructuring to use flux when we can and use teleport-kube-agent instead of custom config
    
    * add teleport in to flux
    
    * testing repo
    
    * repo is fine
    
    * try changing cluster name
    
    * try adding more roles
    
    * set up IAM so we can do k8s
    
    * try removing apps
    
    * try app yaml again
    
    * try bumping logging up and setting role stuff
    
    * try strategicmerge to get annotation on serviceaccount
    
    * argh.  indent, you fool\!
    
    * move teleport so it's all in one place again
    
    * got kubernetes figured out
    
    * fixed teleport-kube-agent
    
    * make sure we are still using the proper branch on main

--- terraform-k8s/teleport.tf
+++ terraform-k8s/teleport.tf
@@ -35,21 +35,46 @@ resource "aws_route53_record" "teleport" {
   records = [data.kubernetes_service.teleport.status.0.load_balancer.0.ingress.0.hostname]
 }
 
-resource "aws_route53_record" "teleport-gitlab" {
+resource "aws_route53_record" "teleport-wildcard" {
   zone_id = data.aws_route53_zone.gitlab.zone_id
-  name    = "gitlab.teleport-${var.cluster_name}"
+  name    = "*.teleport-${var.cluster_name}"
   type    = "CNAME"
   ttl     = "300"
   records = [data.kubernetes_service.teleport.status.0.load_balancer.0.ingress.0.hostname]
 }
 
+# This is the join token
+resource "random_password" "join-token" {
+  length           = 26
+  special          = true
+  override_special = "/@£$"
+}
+
+resource "kubernetes_secret" "teleport-kube-agent-join-token" {
+  depends_on = [kubernetes_namespace.teleport]
+  metadata {
+    name = "teleport-kube-agent-join-token"
+    namespace = "teleport"
+  }
+
+  data = {
+    auth-token = random_password.join-token.result
+  }
+}
+
+# Ideally, this would be done through flux, but we need it to be live
+# so we can reference the service to get the elb to put the CNAMEs on.
 resource "helm_release" "teleport-cluster" {
   name       = "teleport-cluster"
-  repository = "https://charts.releases.teleport.dev"
+  # XXX remove the tspencer repo and add teleport back once these PRs get in:
+  #  https://github.com/gravitational/teleport/pull/6586
+  #  https://github.com/gravitational/teleport/pull/6619
+  # repository = "https://charts.releases.teleport.dev"
+  repository = "https://timothy-spencer.github.io/helm-charts"
   chart      = "teleport-cluster"
   version    = "6.0.0"
   namespace  = "teleport"
-  depends_on = [kubernetes_namespace.teleport]
+   depends_on = [kubernetes_secret.teleport-kube-agent-join-token]
 
   set {
     name  = "namespace"
@@ -61,59 +86,131 @@ resource "helm_release" "teleport-cluster" {
     value = "true"
   }
 
+  # # XXX temporary
+  # set {
+  #   name  = "logLevel"
+  #   value = "DEBUG"
+  # }
+
   set {
     name  = "acmeEmail"
-    value = "security@login.gov"
+    value = var.certmanager-issuer
   }
-
+ 
   set {
     name  = "clusterName"
     value = "teleport-${var.cluster_name}.${var.domain}"
   }
 
   set {
-    name  = "customConfig"
-    value = "true"
+    name  = "kubeClusterName"
+    value = "teleport-${var.cluster_name}"
+  }
+
+ set {
+    name  = "serviceAccountAnnotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.teleport.arn
+  }
+ }
+
+resource "helm_release" "teleport-kube-agent" {
+  name       = "teleport-kube-agent"
+  # XXX remove the tspencer repo and add teleport back once these PRs get in:
+  #  https://github.com/gravitational/teleport/pull/6586
+  #  https://github.com/gravitational/teleport/pull/6619
+  # repository = "https://charts.releases.teleport.dev"
+  repository = "https://timothy-spencer.github.io/helm-charts"
+  chart      = "teleport-kube-agent"
+  version    = "0.0.4"
+  namespace  = "teleport"
+  # XXX temporary
+  wait       = false
+  depends_on = [kubernetes_secret.teleport-kube-agent-join-token]
+
+  set {
+    name  = "namespace"
+    value = "teleport"
+  }
+
+  set {
+    name  = "roles"
+    value = "app"
+  }
+
+  # # XXX temporary
+  # set {
+  #   name  = "logLevel"
+  #   value = "DEBUG"
+  # }
+
+  set {
+    name  = "proxyAddr"
+    value = "teleport-${var.cluster_name}.${var.domain}:443"
+  }
+
+  set {
+    name  = "apps[0].name"
+    value = "gitlab"
+  }
+
+  set {
+    name  = "apps[0].uri"
+    value = "http://gitlab-webservice-default.gitlab:8181"
+  }
+
+  set {
+    name  = "serviceAccountAnnotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.teleport.arn
   }
 }
 
-# This is where the customConfig lives (same name as the helm release)
-resource "kubernetes_config_map" "teleport-cluster" {
-  # depends_on = [helm_release.teleport-cluster]
-  depends_on = [kubernetes_namespace.teleport]
-  metadata {
-    name      = "teleport-cluster"
-    namespace = "teleport"
-  }
 
-  data = {
-    "teleport.yaml" = <<CUSTOMCONFIG
-teleport:
-  log:
-    severity: ERROR
-    output: stdout
-    format: [level, component, caller]
-auth_service:
-  enabled: true
-  cluster_name: teleport-${var.cluster_name}.${var.domain}
-app_service:
-  enabled: true
-  apps:
-    - name: gitlab
-      uri: "http://gitlab-webservice-default.gitlab:8181"
-kubernetes_service:
-  enabled: true
-  listen_addr: 0.0.0.0:3027
-proxy_service:
-  enabled: true
-  public_addr: 'teleport-${var.cluster_name}.${var.domain}:443'
-  kube_listen_addr: 0.0.0.0:3026
-  acme:
-    enabled: true
-    email: ${var.certmanager-issuer}
-ssh_service:
-  enabled: false
+# set things up for the serviceaccount to have proper perms
+resource "aws_iam_role" "teleport" {
+  name               = "${var.cluster_name}-teleport"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${var.oidc_arn}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "ForAnyValue:StringEquals": {
+          "${var.oidc_url}:sub": [
+            "system:serviceaccount:teleport:teleport-kube-agent",
+            "system:serviceaccount:teleport:teleport-cluster"
+          ]
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
 
-CUSTOMCONFIG
-  }
+resource "aws_iam_role_policy" "teleport" {
+  name = "${var.cluster_name}-teleport-policy"
+  role = aws_iam_role.teleport.id
+
+  # This came from https://goteleport.com/docs/aws-oss-guide/#create-iam-policy-granting-list-clusters-and-describe-cluster-permissions-optional
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ListDescribeClusters",
+            "Effect": "Allow",
+            "Action": [
+                "eks:DescribeCluster",
+                "eks:ListClusters"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
