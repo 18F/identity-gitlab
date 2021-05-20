748b80cf27095fe685ec096b0c9feae895ac69d8
timothy-spencer <timothy.spencer@gsa.gov>
2021-05-20 16:22:18 -0700

    get minimal set to let us in to git-ssh

--- terraform/gitlab.tf
+++ terraform/gitlab.tf
@@ -226,33 +226,13 @@ resource "aws_security_group" "gitlab-ingress" {
     ipv6_cidr_blocks = data.aws_ip_ranges.ec2.ipv6_cidr_blocks
   }
 
-  # allow us to be able to get in
+  # allow kubernetes port-forward in to git-ssh
   ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id]
   }
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = [var.vpc_cidr]
-  # }
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # egress {
-  #   from_port        = 0
-  #   to_port          = 0
-  #   protocol         = "-1"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  #   ipv6_cidr_blocks = ["::/0"]
-  # }
 
   tags = {
     Name = "${var.cluster_name}-gitlab-ingress"
