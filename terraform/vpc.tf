1d799f2ac61c6fbd070dfe375610d19d42cbf822
timothy-spencer <timothy.spencer@gsa.gov>
2021-05-21 17:29:10 -0700

    get NAT going

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -29,20 +29,35 @@ resource "aws_subnet" "service" {
   }
 }
 
+# Public subnets to land loadbalancers/NAT/etc.
+resource "aws_subnet" "public_eks" {
+  count = var.service_subnet_count
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + var.service_subnet_count)
+  vpc_id                  = aws_vpc.eks.id
+  map_public_ip_on_launch = true
+
+  tags = map(
+    "Name", "${var.cluster_name}-public-${count.index}",
+    "kubernetes.io/cluster/${var.cluster_name}", "shared",
+    "kubernetes.io/role/elb", "1",
+  )
+}
+
 # have the eks subnets come last so that we can add more later.
 resource "aws_subnet" "eks" {
   count = var.eks_subnet_count
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = cidrsubnet(var.vpc_cidr, 6, count.index + var.service_subnet_count)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 6, count.index + 2)
   vpc_id                  = aws_vpc.eks.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = map(
     "Name", "${var.cluster_name}-node-${count.index}",
     "kubernetes.io/cluster/${var.cluster_name}", "shared",
-    "kubernetes.io/role/elb", "1",
-    "kubernetes.io/role/internal-elb", ""
+    "kubernetes.io/role/internal-elb", "1"
   )
 }
 
@@ -54,7 +69,7 @@ resource "aws_internet_gateway" "eks" {
   }
 }
 
-resource "aws_route_table" "eks" {
+resource "aws_route_table" "public_eks" {
   vpc_id = aws_vpc.eks.id
 
   route {
@@ -67,9 +82,46 @@ resource "aws_route_table" "eks" {
   }
 }
 
-resource "aws_route_table_association" "eks" {
-  count = 2
+resource "aws_route_table_association" "public_eks" {
+  count = var.service_subnet_count
 
-  subnet_id      = aws_subnet.eks.*.id[count.index]
-  route_table_id = aws_route_table.eks.id
+  subnet_id      = aws_subnet.public_eks.*.id[count.index]
+  route_table_id = aws_route_table.public_eks.id
+}
+
+resource "aws_eip" "nat_gateway" {
+  count = var.service_subnet_count
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = var.service_subnet_count
+
+  allocation_id = aws_eip.nat_gateway.*.id[count.index]
+  subnet_id     = aws_subnet.public_eks.*.id[count.index]
+
+  tags = {
+    Name = "${var.cluster_name} NAT ${count.index}"
+  }
+}
+
+resource "aws_route_table" "eks" {
+  count = var.eks_subnet_count
+
+  vpc_id = aws_vpc.eks.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.*.id[count.index]
+  }
+
+  tags = {
+    Name = "${var.cluster_name} route to NAT ${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "eks" {
+  count = var.eks_subnet_count
+
+  subnet_id     = aws_subnet.eks.*.id[count.index]
+  route_table_id = aws_route_table.eks.*.id[count.index]
 }
