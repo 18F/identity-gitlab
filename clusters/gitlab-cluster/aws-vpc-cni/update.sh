d9665181e937b9ef7ff54923f2f708f607ff0077
Tim Spencer <timothy.spencer@gsa.gov>
2021-06-24 10:14:04 -0700

    Updating stuff (#40)
    * Add Flux v0.15.0 component manifests
    
    * Add Flux sync manifests
    
    * added documentation, fixed branch/git pull method up.
    
    * update new versions of everything except gitlab
    
    * cannot upgrade node termination handler right now: https://github.com/aws/eks-charts/issues/546
    
    Co-authored-by: Flux <>

--- clusters/gitlab-cluster/aws-vpc-cni/update.sh
+++ clusters/gitlab-cluster/aws-vpc-cni/update.sh
@@ -2,6 +2,11 @@
 #
 # update the aws-vpc-cni!
 #
+# You can find what releases are available by going here:
+# https://github.com/aws/amazon-vpc-cni-k8s/releases
+# Then find the url that they have for deploying the release
+# you want and put it in here.
+#
 
 curl https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/v1.8.0/config/v1.8/aws-k8s-cni.yaml > aws-k8s-cni.yaml
 
