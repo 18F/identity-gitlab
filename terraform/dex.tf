c69e2a648cf28fabb20893e8ebdb5cf337451a36
timothy-spencer <timothy.spencer@gsa.gov>
2021-06-25 10:48:48 -0700

    changed pivproxy stuff to be helm-ified so that we could pass in certs and hostnames

--- terraform/dex.tf
+++ terraform/dex.tf
@@ -24,6 +24,7 @@ resource "kubernetes_config_map" "pivproxy-config" {
     "uid_list"      = join(", ", formatlist("\"%s\"", var.uid_list))
     "cert-arn"      = aws_acm_certificate.dex.arn
     "pivproxy_name" = local.pivproxyname
+    "pivproxy-cert-arn"      = aws_acm_certificate.pivproxy.arn
   }
 }
 
@@ -57,4 +58,36 @@ resource "aws_route53_record" "dex-validation" {
 resource "aws_acm_certificate_validation" "dex" {
   certificate_arn         = aws_acm_certificate.dex.arn
   validation_record_fqdns = [for record in aws_route53_record.dex-validation : record.fqdn]
+}
+
+# cert for pivproxy, attached to the network lb
+resource "aws_acm_certificate" "pivproxy" {
+  domain_name       = local.pivproxyname
+  validation_method = "DNS"
+
+  tags = {
+    Name = local.pivproxyname
+  }
+}
+
+resource "aws_route53_record" "pivproxy-validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.pivproxy.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.gitlab.zone_id
+}
+
+resource "aws_acm_certificate_validation" "pivproxy" {
+  certificate_arn         = aws_acm_certificate.pivproxy.arn
+  validation_record_fqdns = [for record in aws_route53_record.pivproxy-validation : record.fqdn]
 }
